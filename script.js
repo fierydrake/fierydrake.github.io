26c83d72ed4827ae23e8ff263abb2663f99762aa
fierydrake <m.j.tunnicliffe@gmail.com>
2014-10-14 01:01:35 +0100

    First build of blast

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ $(function() {
   $("#games ul li:not(.nogame)").hover(function(e) {
     $(this).css('cursor', 'pointer');
   });
-});
+
+  $("#games .project a").css('text-decoration', 'none');
+  $("#games .project").click(function(e) {
+    window.location.href= $(this).find("span.name a").attr("href");
+  });
+  $("#games .project").hover(function(e) {
+    $(this).css('cursor', 'pointer');
+  });});
